ad681db76388e2085e1985bdd75dae8e8ca60e40
Mauro de Pascale <mauro.depascale.work@outlook.it>
2023-04-20 15:51:21 +0200

    Adding code to retrieve subnet id

--- lambdaCode/lambdaCode.py
+++ lambdaCode/lambdaCode.py
@@ -1,8 +1,17 @@
 import requests
 import json
+import boto3
 
 def lambda_handler(event, context):
   api_url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+  
+  client = boto3.client('lambda')
+  response = client.get_function_configuration(
+    FunctionName='lambda_handler'
+  )
+
+  print(response)
+  
   payload = {
         "subnet_id": "TBD",
         "name": "Mauro de Pascale",
