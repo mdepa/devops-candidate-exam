d6cfbe70b3e1f4648c1174ca0fe77b47ea61d007
Mauro de Pascale <mauro.depascale.work@outlook.it>
2023-04-20 20:11:04 +0200

    Trying to retrieve subnetID

--- lambdaCode/lambdaCode.py
+++ lambdaCode/lambdaCode.py
@@ -14,11 +14,11 @@ def lambda_handler(event, context):
   #  Qualifier='1'
   )
 
-  print(response)
+  #print(response)
   print(json.dumps(response, indent=2, default=str))
-  print(response['VpcConfig']['SubnetIds'][0])
+  #print(response['VpcConfig']['SubnetIds'][0])
   payload = {
-        "subnet_id": "TBD",
+        "subnet_id": response['VpcConfig']['SubnetIds'][0],
         "name": "Mauro de Pascale",
         "email": "mauro.de_pascale@siemens.com"
       }
@@ -28,7 +28,7 @@ def lambda_handler(event, context):
   print('done')
 
   
-  print(base64.b64decode(response.text))
+  print(base64.b64decode(response.text + b'=='))
   #print(json.dumps(response.json(), indent=4))
   #print(response.status_code)
   #return { 
